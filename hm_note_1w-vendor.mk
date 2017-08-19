754c369de08d81db950cd61ff4c8961ae5f8c92a
EndLess728 <aryanmk.mk5@gmail.com>
2017-08-19 19:12:49 +0530

    AOSP Bringup

---
+++ hm_note_1w-vendor.mk
@@ -0,0 +1,10 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Xiaomi/hm_note_1w/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Xiaomi/hm_note_1w/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Xiaomi/hm_note_1w/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Xiaomi/hm_note_1w/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Xiaomi/hm_note_1w/xbin,system/xbin)
+
